99d173e9e82389376a695528790c1c82b743b6f6
Rama Ramana Sharma Parnandi <ramaramana20021212@gmail.com>
2025-07-22 21:52:28 +0530

    Day 23

---
+++ turtle-crossing-day-23/car_manager.py
@@ -0,0 +1,29 @@
+import random
+from turtle import Turtle
+
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
+
+
+class CarManager(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.all_cars = []
+        self.hideturtle()
+
+    def create_car(self):
+        random_chance = random.randint(1,6)
+        if random_chance == 1:
+            new_car = Turtle("square")
+            new_car.shapesize(1,2)
+            new_car.penup()
+            new_car.color(random.choice(COLORS))
+            new_car.setheading(180)
+            random_y = random.randint(-250, 250)
+            new_car.goto(299, random_y)
+            self.all_cars.append(new_car)
+
+    def move_cars(self):
+        for car in self.all_cars:
+            car.forward(STARTING_MOVE_DISTANCE)
