a0b39eb75a3e6b3c8ee25ba70ff4b9267addfca5
Rama Ramana Sharma Parnandi <ramaramana20021212@gmail.com>
2025-08-19 21:22:07 -0400

    Implementing Exception Handling

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -1,12 +1,16 @@
 import pandas as pd
 
-#TODO 1. Create a dictionary in this format:
-#{"A": "Alfa", "B": "Bravo"}
 data = pd.read_csv('nato_phonetic_alphabet.csv')
 phonetic_dict = {row.letter:row.code for (index, row) in data.iterrows()}
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input("Enter a word: ").upper()
-output = [phonetic_dict[alpha] for alpha in word]
-print(output)
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output = [phonetic_dict[alpha] for alpha in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet are allowed.")
+        generate_phonetic()
+    else:
+        print(output)
 
+generate_phonetic()
