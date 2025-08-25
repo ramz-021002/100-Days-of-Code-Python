6a1d3f6f11583a2101656e3541d49126d91f1443
Rama Ramana Sharma Parnandi <ramaramana20021212@gmail.com>
2025-08-24 22:20:21 -0400

    Fixing HTML formatting in API response

--- quizzler-app/ui.py
+++ quizzler-app/ui.py
@@ -1,2 +1,14 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
+
+class QuizInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzer")
+        self.window.geometry("400x400")
+        self.window.configure(background=THEME_COLOR)
+
+        self.window.mainloop()
 
