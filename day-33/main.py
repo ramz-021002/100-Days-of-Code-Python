423d4b30dfcae0a98f8b79dd9f698817fb3bdaa3
Rama Ramana Sharma Parnandi <ramaramana20021212@gmail.com>
2025-08-24 21:43:57 -0400

    End of Day 33

--- day-33/main.py
+++ day-33/main.py
@@ -1,12 +1,63 @@
 import requests
+from datetime import datetime
+import smtplib
+import time
 
-response = requests.get(url='http://api.open-notify.org/iss-now.json')
-response.raise_for_status()
+MY_LAT = 37.2431
+MY_LOG = 115.7930
+EMAIL_ADDRESS = '<EMAIL>'
+PASSWORD = '<PASSWORD>'
 
-data = response.json()
+def compare_locations():
+    response = requests.get(url='http://api.open-notify.org/iss-now.json')
+    response.raise_for_status()
+    
+    data = response.json()
+    
+    latitude = float(data['iss_position']['latitude'])
+    longitude = float(data['iss_position']['longitude'])
+    print(latitude, longitude)
+    if (MY_LAT - 5 <= latitude <= MY_LAT + 5) and (MY_LOG - 5 <= latitude <= MY_LOG + 5):
+        return True
+    else:
+        return False
 
-latitude = data['iss_position']['latitude']
-longitude = data['iss_position']['longitude']
+def is_dark():
+    time_now = datetime.now()
+    current_hour = time_now.hour
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LOG,
+        "formatted": 0,
+        "tzid": "America/New_York"
+    }
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
-iss_position = (latitude, longitude)
+    if current_hour >= sunset or current_hour <= sunrise:
+        return True
+    else:
+        return False
 
+def send_email(message):
+    with smtplib.SMTP('smtp.gmail.com', 587) as connection:
+        connection.starttls()
+
+        connection.login(EMAIL_ADDRESS, PASSWORD)
+        connection.sendmail(
+            from_addr=EMAIL_ADDRESS,
+            to_addrs="<EMAIL>",
+            msg=f"Subject: 'Look for ISS'\n\n {message}"
+        )
+
+
+
+while True:
+    if compare_locations() & is_dark():
+        send_email("Look up on the sky for ISS")
+    else:
+        print("Look Down")
+    time.sleep(60)
